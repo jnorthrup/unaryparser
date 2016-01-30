d8ccf3914b4f68d0f9d650ad58154e28921ca215
jim <jim@vsiwest.com>
2016-01-30 04:00:46 -0800

    stmt parser needs fixing

--- src/test/java/sun/misc/unreal/nars/narsTest.java
+++ src/test/java/sun/misc/unreal/nars/narsTest.java
@@ -4,6 +4,7 @@ import bbcursive.std;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertNotNull;
+import static sun.misc.unreal.nars.nars.statement;
 
 /**
  * Created by jim on 1/27/16.
@@ -13,7 +14,7 @@ public class narsTest {
     @Test
     public void testsmt() throws Exception {
         std.flags.get().add(std.traits.backtrackOnNull);
-        assertNotNull(std.bb("<chess --> competition>", nars.statement()));
+        assertNotNull(std.bb("<chess --> competition>", statement()));
     }
 
     @Test
